a6018122e4ea800ebe851768a4357af3cba8849c
nicoleCastagno <nicole@remente.com>
2017-05-20 22:23:02 +0200

    Connect MVP interfaces for the accounts screen to the MainActivity

--- app/src/main/java/com/bundle/waja/MainActivity.kt
+++ app/src/main/java/com/bundle/waja/MainActivity.kt
@@ -2,11 +2,24 @@ package com.bundle.waja
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import butterknife.bindView
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
+import com.bundle.waja.home.AccountsController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var router: Router
+    val container : ViewGroup by bindView(R.id.layoutRoot)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        router = Conductor.attachRouter(this, container, savedInstanceState)
+        if (!router.hasRootController()){
+            router.setRoot(RouterTransaction.with(AccountsController()))
+        }
     }
 }
