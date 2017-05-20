e72c820224bbe8eb7309dd9e7202aabaea145fbd
nicoleCastagno <nicole@remente.com>
2017-05-20 22:21:35 +0200

    Implement MVP intefaces for accounts screen

---
+++ app/src/main/java/com/bundle/waja/home/AccountModel.kt
@@ -0,0 +1,4 @@
+package com.bundle.waja.home
+
+data class AccountModel(val accountName: String, val accountEmail: String)
+
---
+++ app/src/main/java/com/bundle/waja/home/AccountsPresenter.kt
@@ -0,0 +1,6 @@
+package com.bundle.waja.home
+
+import com.bundle.waja.common.MVP
+
+class AccountsPresenter : MVP.Presenter<AccountScreen>(){}
+
