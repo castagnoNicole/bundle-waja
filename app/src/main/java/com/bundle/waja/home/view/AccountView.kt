bae7a05b9d562f8ce749bdcf6d87a8f303ad690d
nicoleCastagno <nicole@remente.com>
2017-05-21 13:09:59 +0200

    Create view for the single Account

---
+++ app/src/main/java/com/bundle/waja/home/view/AccountView.kt
@@ -0,0 +1,27 @@
+package com.bundle.waja.home.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import android.widget.TextView
+import butterknife.bindView
+import com.bundle.waja.R
+import com.bundle.waja.home.AccountModel
+
+class AccountView: LinearLayout {
+    private val accountName: TextView by bindView(R.id.acount_name)
+    private val accountEmail: TextView by bindView(R.id.acount_mail)
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes)
+    constructor(context: Context, attributes: AttributeSet, defStyle: Int) : super(context, attributes, defStyle)
+
+    init {
+        inflate(context, R.layout.view_account, this)
+    }
+
+    fun showAccount(account: AccountModel) {
+        accountName.text = account.name
+        accountEmail.text = account.email
+    }
+}
