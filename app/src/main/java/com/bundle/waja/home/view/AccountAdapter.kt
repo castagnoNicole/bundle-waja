76b0a5829fdd855e269249d11f8621a9ba10beda
nicoleCastagno <nicole@remente.com>
2017-05-21 15:21:09 +0200

    Refractor AccountAdapter

--- app/src/main/java/com/bundle/waja/home/view/AccountAdapter.kt
+++ app/src/main/java/com/bundle/waja/home/view/AccountAdapter.kt
@@ -2,21 +2,19 @@ package com.bundle.waja.home.view
 
 import android.graphics.Color
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import com.bundle.waja.home.AccountModel
 
-class AccountAdapter(var accounts: List<AccountModel>): RecyclerView.Adapter<AccountAdapter.ViewHolder>() {
-    open class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+class AccountAdapter(var accounts: List<AccountModel>): RecyclerView.Adapter<AccountAdapter.AccountViewHolder>() {
 
-    class AccountViewHolder(private val view: AccountView) : ViewHolder(view){
+    class AccountViewHolder(private val view: AccountView) : RecyclerView.ViewHolder(view){
         fun bindAccount(account: AccountModel){
             view.showAccount(account)
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountViewHolder {
         val view = AccountView(parent.context).apply {
             layoutParams = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
             setBackgroundColor(Color.LTGRAY)
@@ -25,10 +23,8 @@ class AccountAdapter(var accounts: List<AccountModel>): RecyclerView.Adapter<Acc
         return AccountViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val account = accounts[position]
-        if (holder is AccountViewHolder)
-            holder.bindAccount(account)
+    override fun onBindViewHolder(holder: AccountViewHolder, position: Int) {
+            holder.bindAccount(accounts[position])
     }
 
     override fun getItemCount() = accounts.size
