d6f35f2625cc62069c24c2499d3dcfb651df7235
nicoleCastagno <nicole@remente.com>
2017-05-23 09:03:26 +0200

    Show account details view from the controller

--- app/src/main/java/com/bundle/waja/details/view/AccountDetailsController.kt
+++ app/src/main/java/com/bundle/waja/details/view/AccountDetailsController.kt
@@ -3,19 +3,27 @@ package com.bundle.waja.details.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.bluelinelabs.conductor.Controller
+import com.bundle.waja.R
 import com.bundle.waja.common.MVP
+import com.bundle.waja.details.presenter.AccountDetailsPresenter
 
 interface AccountDetailsScreen :MVP.View {
     fun showEditAccount()
 }
 
 class AccountDetailsController : Controller(), AccountDetailsScreen{
-    override fun showEditAccount() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private var presenter = AccountDetailsPresenter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        presenter.view = this
+        val view = inflater.inflate(R.layout.account_details, container, false)
+
+        return view
+    }
+
+    override fun showEditAccount() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
