0c2afecaacbed9d7fd58a403a5293bc995df742d
nicoleCastagno <nicole@remente.com>
2017-06-21 16:09:57 +0200

    Change the account model

---
+++ app/src/main/java/com/bundle/waja/home/data/RealmAccount.kt
@@ -0,0 +1,5 @@
+package com.bundle.waja.home.data
+
+import io.realm.RealmObject
+
+//class RealmAccount(val emaail: String, val account: String, val password: String, val type: Int): RealmObject()
--- app/src/main/java/com/bundle/waja/home/view/AccountAdapter.kt
+++ app/src/main/java/com/bundle/waja/home/view/AccountAdapter.kt
@@ -3,10 +3,19 @@ package com.bundle.waja.home.view
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import com.bundle.waja.R
 import com.bundle.waja.home.AccountModel
 
-class AccountAdapter(var accounts: List<AccountModel>): RecyclerView.Adapter<AccountAdapter.AccountViewHolder>() {
+class AccountAdapter(): RecyclerView.Adapter<AccountAdapter.AccountViewHolder>() {
     var onAccountClicked: ((AccountModel) -> Unit)? = null
+    val accounts =
+            listOf(
+                    AccountModel("Pino Caluso", "pippi@gmail.it", "password", R.drawable.ic_mood_fancy_inverted_5),AccountModel("Facebook", "gino@caluso.com", "password", R.drawable.ic_mood_fancy_inverted_5),
+                    AccountModel("Facebook", "pippi@gmail.it", "password", R.drawable.ic_mood_fancy_inverted_5),AccountModel("Facebook", "gino@caluso.com", "password", R.drawable.ic_mood_fancy_inverted_5),
+                    AccountModel("Facebook", "pippi@gmail.it", "password", R.drawable.ic_mood_fancy_inverted_5),AccountModel("Facebook", "gino@caluso.com", "password", R.drawable.ic_mood_fancy_inverted_5),
+                    AccountModel("Facebook", "pippi@gmail.it", "password", R.drawable.ic_mood_fancy_inverted_5),AccountModel("Facebook", "gino@caluso.com", "password", R.drawable.ic_mood_fancy_inverted_5),
+                    AccountModel("Facebook", "pippi@gmail.it", "password", R.drawable.ic_mood_fancy_inverted_5),AccountModel("Facebook", "gino@caluso.com", "password", R.drawable.ic_mood_fancy_inverted_5)
+            )
 
     class AccountViewHolder(private val view: AccountView, var onClick: (AccountModel)->Unit) : RecyclerView.ViewHolder(view){
         fun bindAccount(account: AccountModel){
